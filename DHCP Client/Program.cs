2d92aa3809c72f80651d0d63de51e83de5c4df71
QuocViet-uth <viettlq3472@ut.edu.vn>
2025-08-04 19:08:49 +0700

    Add project files.

---
+++ DHCP Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;
+
+class DhcpClient
+{
+    static void Main()
+    {
+        Console.WriteLine("Yeu cau cap lai dia chi IP moi...\n");
+
+        ProcessStartInfo psi = new ProcessStartInfo("ipconfig", "/renew")
+        {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        Process process = new Process { StartInfo = psi };
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        Console.WriteLine(output);
+    }
+    static bool IsRunAsAdmin()
+    {
+        WindowsIdentity id = WindowsIdentity.GetCurrent();
+        WindowsPrincipal principal = new WindowsPrincipal(id);
+        return principal.IsInRole(WindowsBuiltInRole.Administrator);
+    }
+}
